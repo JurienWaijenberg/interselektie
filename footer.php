62e06a90ca26b172f4e85eeb669b92d60ac6a4c4
Juriën Waijenberg <jurien.waijenberg@hva.nl>
2024-06-15 10:59:03 +0200

    Added header and footer

--- footer.php
+++ footer.php
@@ -1,8 +1,123 @@
 </main>
 
-<footer>
+<section class="blue-bg">
+			<footer class="max-width">
+				
+				<section class="footer-top">
+					
+						<h2>Interselektie. <span>Altijd de juiste connectie</span></h2>
+				
+					<section>
+						<section>
+							<h3>Ik zoek een baan</h3>
+							<ul>
+								<li><a href=""><p>Vacatures</p></a></li>
+								<li><a href=""><p>Vacatures per vakgebied</p></a></li>
+								<li><a href=""><p>Vacatures per regio</p></a></li>
+								<li><a href=""><p>Sollicitatieprocedure</p></a></li>
+								<li><a href=""><p>Opleiding Training</p></a></li>
+								<li><a href=""><p>Kenniscentrum</p></a></li>
+							</ul>
+						</section>
 
-</footer>
+						<section>
+							<h3>Voor werkgevers</h3>
+							<ul>
+								<li><a href=""><p>Ik zoek personeel</p></a></li>
+								<li><a href=""><p>voordelen samenwerken Interselektie</p></a></li>
+								<li><a href=""><p>Hoe vind je de juiste personeel’-gids</p></a></li>
+								<li><a href=""><p>Sollicitatieprocedure</p></a></li>
+								<li><a href=""><p>Vacature aanmelden</p></a></li>
+								<li><a href=""><p>Specialisaties</p></a></li>
+							</ul>
+							<h3>Algemene informatie</h3>
+							<ul>
+								<li><a href=""><p>Keurmerken/verenigingen</p></a></li>
+								<li><a href=""><p>BerichtenSelektie</p></a></li>
+								<li><a href=""><p>Documenten</p></a></li>
+							</ul>
+						</section>
+
+						<section class="footer-img">
+							<img src="images/footer-losse-man.png" alt="">
+						</section>
+
+						<section>
+							<h3>Contact</h3>
+							<ul>
+								<li><p>Interselektie B.V <br>
+									Provincialeweg 21-A <br>
+									1561 KL  Krommenie</p></li>
+								<li><a href="">	<p><i class="fa-solid fa-phone-flip"></i> 075 - 621 88 45</p></a></li>
+								<li><a href="">	<p><i class="fa-solid fa-paper-plane"></i> info@interselektie.nl</p></a></li>
+							</ul>
+							<ul>
+								<!--of ook hier iconen-->
+								<li><a href="x"><i class="fa-brands fa-facebook-f"></i></a></li>
+								<li><a href="x"><i class="fa-brands fa-twitter"></i></a></li>
+								<li><a href="x"><i class="fa-brands fa-linkedin-in"></i></a></li>
+								<li><a href="x"><i class="fa-brands fa-youtube"></i></a></li>
+							</ul>
+						</section>
+					</section>
+				</section>
+
+
+				<section class="footer-top-phone">
+					<ul>
+						<li><h3>Contact</h3></li>
+						<li><p>Interselektie B.V <br>
+							Provincialeweg 21-A <br>
+							1561 KL  Krommenie</p></li>
+						<li><a href="">	<p><i class="fa-solid fa-phone-flip"></i> 075 - 621 88 45</p></a></li>
+						<li><a href="">	<p><i class="fa-solid fa-paper-plane"></i> info@interselektie.nl</p></a></li>
+					</ul>
+					<img src="images/footer-losse-man.png" alt="">
+				</section>
+			</section>
+
+			<section class="max-width">
+
+				<section class="footer-bottom-phone">
+					<ul>
+						<li><a href=""><p>Werkzoekend</p></a></li>
+						<li><a href=""><p>Ik zoek een baan</p></a></li>
+						<li><a href=""><p>Voor werkgevers</p></a></li>
+						<li><a href=""><p>Keurmerken/verenigingen</p></a></li>
+						<li><a href=""><p>BerichtenSelektie</p></a></li>
+						<li><a href=""><p>Documenten</p></a></li>
+					</ul>
+					
+
+					<ul>
+						<!--of ook hier iconen-->
+						<li><a href="x"><i class="fa-brands fa-facebook-f"></i></a></li>
+						<li><a href="x"><i class="fa-brands fa-twitter"></i></a></li>
+						<li><a href="x"><i class="fa-brands fa-linkedin-in"></i></a></li>
+						<li><a href="x"><i class="fa-brands fa-youtube"></i></a></li>
+					</ul>
+				</section>
+
+
+				<section class="footer-bottom">
+					<ul>
+						<li><a href=""><p>Disclaimer</p></a></li>
+						<li><p>/</p></li>
+						<li><a href=""><p>Privacy Policy </p></a></li>
+						<li><p>/</p></li>
+						<li><a href=""><p>Algemene Voorwaarden</p></a></li>
+						<li><p>/</p></li>
+						<li><a href=""><p>Antidiscriminatiebeleid </p></a></li>
+						<li><p>/</p></li>
+						<li><a href=""><p>Gedragscode</p></a></li>
+						<li><p>/</p></li>
+						<li><a href=""><p>Cookie statement</p></a></li>
+						<li><p>/</p></li>
+						<li><a href=""><p>Sitemap</p></a></li>
+					</ul>
+				</section>
+			</footer>
+		</section>
 
 <?php wp_footer(); ?>
 </body>
--- header.php
+++ header.php
@@ -10,11 +10,41 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <?php wp_head(); ?>
     <!-- <meta name="theme-color" content="#E63226" /> -->
-
+    <script src="https://kit.fontawesome.com/5c7d6381f7.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <header>
-        
+        <!--header-top-->
+        <section class="rood-bg">
+            <section class="max-width">
+                <ul>
+                    <li><h4>NL</h4><i class="fa-solid fa-chevron-down"></i></li><!--pijl beneden-->
+                    <li><i class="fa-solid fa-heart"></i></li>
+                    <li>
+                        <i class="fa-solid fa-user"></i>
+                        <a href="x">
+                            <h4>Inloggen</h4>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+        </section>
+        <!--header-bottom-->
+        <section class="max-width">
+            
+            <a href="index.html"><i class="fa-solid fa-user"></i></a>
+            <a href="index.html"><img src="http://interselektie.local/wp-content/uploads/2024/06/Logo-Interselektie.png" alt="Logo Interselektie"></a>
+            <a href="index.html"><i class="fa-solid fa-user"></i></a>
+            <nav>
+                <ul>
+                    <li><a href="x"><h4>Over Interselektie</h4><i class="fa-solid fa-chevron-down"></i></a></li>
+                    <li><a href="x"><h4>Werknemer</h4><i class="fa-solid fa-chevron-down"></i></a></li>
+                    <li><a href="x"><h4>Werkgever</h4><i class="fa-solid fa-chevron-down"></i></a></li>
+                    <li><a href="x"><h4>Berichten</h4></a></li>
+                    <li><a href="x"><h4>Contact</h4><i class="fa-solid fa-chevron-down"></i></a></li>
+                </ul>
+            </nav>
+        </section>
     </header>
     <main>
