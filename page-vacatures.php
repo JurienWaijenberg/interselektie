30a14014f8de6fbba86f1f757b33ed1112235bbe
Juriën Waijenberg <jurien.waijenberg@hva.nl>
2024-06-15 12:40:00 +0200

    Added vacatures page

---
+++ page-vacatures.php
@@ -0,0 +1,92 @@
+<?php get_header(); ?>
+
+<!--atf home-->
+<section class="atf-vacatures-bg">
+
+    <section class="atf-home max-width">
+        
+    </section>
+
+    <section class="atf-home-phone">
+
+        <section class="red-bg-form">
+            <form action="/action_page.php">
+                <input type="text" id="functie" placeholder="&#xF002;  Functie" style="font-family:Helvetica, Arial, sans-serif, FontAwesome" />
+                <input type="text" id="plaatsnaam-postcode" placeholder="&#xF002;  Plaatsnaam of postcode" style="font-family:Helvetica, Arial, sans-serif, FontAwesome" />
+                <input type="submit" id="vacatures-zoeken" value="Vacatures zoeken">
+            </form>
+
+            <a href="x"><h4><i class="fa-solid fa-phone-flip"></i> 075 - 621 88 45</h4></a>
+        </section>	
+
+        
+
+    </section>
+
+</section>
+
+<section class="max-width vacatures-intro">
+    <section>
+        <a class="header-dot" href="#">
+            <i class="fa-solid fa-circle"></i>
+            <h2>Vacatures voor jou geselekteerd</h2>
+            <i class="fa-solid fa-chevron-right"></i>
+        </a>
+        <p>
+        Ben jij op zoek naar een nieuwe uitdaging in de productie? Voor een bedrijf gevestigd in Zaanstad is Interselektie op zoek naar een enthousiaste verpakkingsoperator om het  team te versterken! Als operator verpakken ben je verantwoordelijk voor het efficiënt en nauwkeurig verpakken van producten. Je werkt binnen een productieomgeving en zorgt ervoor dat de verpakkingslijn optimaal functioneert…
+        </p>
+    </section>
+    <section>
+        <section>
+            <h2><span>Interselektie</span> <br> Altijd de juiste connectie</h2>
+            
+
+            <form action="/action_page.php">
+                <input type="text" id="functie" placeholder="&#xF002;  Functie" style="font-family:Helvetica, Arial, sans-serif, FontAwesome" />
+                <input type="text" id="plaatsnaam-postcode" placeholder="&#xF002;  Plaatsnaam of postcode" style="font-family:Helvetica, Arial, sans-serif, FontAwesome" />
+                <input type="submit" id="vacatures-zoeken" value="Vacatures zoeken">
+            </form>
+
+            <a href="x"><h4><i class="fa-solid fa-phone-flip"></i> 075 - 621 88 45</h4></a>
+        </section>
+    </section>
+</section>
+
+<!--Top vacatures-->
+<section class="top-vacatures max-width">
+
+    <section>
+        <ul>
+            <li>
+                <h3>Werkvoorbereider/ Calculator elektrotechniek</h3>
+                <p><i class="fa-solid fa-location-dot"></i> Amsterdam </p>
+                <p>Ben jij op zoek naar een nieuwe uitdaging in de productie? Interselektie is op zoek naar een enthousiaste verpakkingsoperator…</p>
+                <a href="x">Bekijk vacature</a>
+                <a href="x">Bewaar vacature <i class="fa-regular fa-heart"></i></a>
+            </li>
+        </ul>
+
+        <ul>
+            <li>
+                <h3>Werkvoorbereider/ Calculator elektrotechniek</h3>
+                <p><i class="fa-solid fa-location-dot"></i> Amsterdam </p>
+                <p>Ben jij op zoek naar een nieuwe uitdaging in de productie? Interselektie is op zoek naar een enthousiaste verpakkingsoperator…</p>
+                <a href="x">Bekijk vacature</a>
+                <a href="x">Bewaar vacature <i class="fa-regular fa-heart"></i></a>
+            </li>
+        </ul>
+
+        <ul>
+            <li>
+                <h3>Werkvoorbereider/ Calculator elektrotechniek</h3>
+                <p><i class="fa-solid fa-location-dot"></i> Amsterdam </p>
+                <p>Ben jij op zoek naar een nieuwe uitdaging in de productie? Interselektie is op zoek naar een enthousiaste verpakkingsoperator…</p>
+                <a href="x">Bekijk vacature</a>
+                <a href="x">Bewaar vacature <i class="fa-regular fa-heart"></i></a>
+            </li>
+        </ul>
+    </section>
+
+</section>
+
+<?php get_footer(); ?>
