c2b5e1493adc17f8b58b5e03a57801ca7ed1f182
TessaHeijnen <tessaheijne0709@gmail.com>
2024-06-15 13:50:52 +0200

    atf + intro tekst + uitzenden etc cards start

--- page-werkgever.php
+++ page-werkgever.php
@@ -1,1 +1,91 @@
-<h1>Werkgever</h1>
+<?php get_header(); ?>
+
+<section class="atf-bg-werkgever">
+
+	<section class="atf-werkgever max-width">
+		<h1>Ik zoek <br> personeel</h1>
+		<section>
+			<h2><span>Interselektie</span> <br> Altijd de juiste connectie</h2>
+
+             <a href="x">Vacature aanmelden</a>
+
+			<a href="x"><h4><i class="fa-solid fa-phone-flip"></i> 075 - 621 88 45</h4></a>
+		</section>
+	</section>
+
+		<section class="atf-werkgever-phone">
+            <h1>Ik zoek <br> personeel</h1>
+	
+			<section class="red-bg-form">
+				<article>
+                    <h2><span>Interselektie</span> <br> Altijd de juiste connectie</h3>
+                    <a href="x">Vacature aanmelden</a>
+                </article>
+
+				<a href="x"><h4><i class="fa-solid fa-phone-flip"></i> 075 - 621 88 45</h4></a>
+			</section>	
+		</section>
+</section>        
+
+<section class="introWerkgever max-width">
+	<!-- <h2>Onze diensten <i class="fa-solid fa-circle"></i> </h2> -->
+    <div>
+        <i class="fa-solid fa-circle"></i>
+        <h2>Onze diensten</h2>
+    </div>
+    <p>
+        Of u nu op zoek bent naar flexibele arbeidskrachten voor korte of lange termijn, behoefte heeft aan gespecialiseerde professionals voor specifieke projecten, of juist op zoek bent naar de perfecte match voor een vaste positie binnen uw organisatie, wij zijn er om u te ondersteunen. Ontdek hoe onze expertise en persoonlijke aanpak kunnen bijdragen aan het succes van uw bedrijf. 
+    </p>
+</section>
+
+
+<!-- <section class="werkgeverCards max-width">
+
+</section> -->
+
+
+<section class="werkgeverCards max-width">
+    <ul>
+		<li>
+            <img src="http://interselektie.local/wp-content/uploads/2024/06/lachende-vrouw.jpg" alt="">
+            <div>
+                <h3>Uitzenden</h3>
+                <p>Heeft u snel gemotiveerde flexmedewerkers nodig? Dan biedt uitzenden de oplossing. Laten we samen kijken hoe uitzenden u 
+                    kan helpen!</p>
+                <a href=""><p>Meer informatie</p></a>
+            </div>
+		</li>
+
+		<li>
+            <img src="http://interselektie.local/wp-content/uploads/2024/06/lachende-vrouw.jpg" alt="">
+            <div>
+                <h3>Detacheren</h3>
+                <p>Heeft u voor langere tijd een professional nodig, maar wilt u geen risico’s lopen? Dan biedt detacheren de oplossing. Laten we samen kijken hoe detacheren u kan helpen!</p>
+                <a href=""><p>Meer informatie</p></a>
+            </div>
+		</li>
+
+		<li>
+            <img src="http://interselektie.local/wp-content/uploads/2024/06/lachende-vrouw.jpg" alt="">
+            <div>
+                <h3>Werving en Selectie</h3>
+                <p>Geen tijd om zelf personeel te werven? 
+                Dan biedt werving en selectie de oplossing. Laten we samen kijken hoe werving en selectie u kan helpen!</p>
+                <a href=""><p>Lees verder</p></a>
+            </div>
+		</li>
+	</ul>
+</section>
+
+
+
+		
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
